a23a78dd50b12dc36a716e7027e81fddb60e2f46
luoji <gunsluo@gmail.com>
2015-03-17 23:55:48 +0800

    [feature] (admin) customer login and register success Signed-off-by: luoji <gunsluo@gmail.com>

--- src/admin/actions/AdminAction.class.php
+++ src/admin/actions/AdminAction.class.php
@@ -4,31 +4,30 @@
 
 class AdminAction extends AjaxAction
 {
-	public function login(){
-		$usr = isset($_POST["usrname"]) ? $_POST["usrname"] : null;
-		$pwd = isset($_POST["usrpwd"]) ? $_POST["usrpwd"] : null;
-		$json['status'] = ErrorCode::E_SUCCESS;
-		//$session = new Session();
-		$json = new stdClass();
-		$entity = new AdminEntity();
-		if(empty($usr) || empty($pwd))
-		{
-			$json->status = ErrorCode::E_LOGIN_FAIL;
-			$json->msg = ErrorCode::getErrDesc(ErrorCode::E_LOGIN_FAIL);
-			echo json_encode($json, true);
-			return;
-		}
+    public function login(){
+        $usr = isset($_POST["usrname"]) ? $_POST["usrname"] : null;
+        $pwd = isset($_POST["usrpwd"]) ? $_POST["usrpwd"] : null;
+        //$session = new Session();
+        $json = new stdClass();
+        $entity = new AdminEntity();
+        if(empty($usr) || empty($pwd))
+        {
+            $json->status = ErrorCode::E_LOGIN_FAIL;
+            $json->msg = ErrorCode::getErrDesc(ErrorCode::E_LOGIN_FAIL);
+            echo json_encode($json, true);
+            return;
+        }
 
-		if($entity->auth($usr, $pwd) == 1)
-		{
-			$json->status = ErrorCode::E_SUCCESS;
-			$json->msg = ErrorCode::getErrDesc(ErrorCode::E_SUCCESS);
-		}
-		else {
-			$json->status = ErrorCode::E_LOGIN_FAIL;
-			$json->msg = ErrorCode::getErrDesc(ErrorCode::E_LOGIN_FAIL);
-		}
-		echo json_encode($json);
-	}
+        if($entity->auth($usr, $pwd) === 1)
+        {
+            $json->status = ErrorCode::E_SUCCESS;
+            $json->msg = ErrorCode::getErrDesc(ErrorCode::E_SUCCESS);
+        }
+        else {
+            $json->status = ErrorCode::E_LOGIN_FAIL;
+            $json->msg = ErrorCode::getErrDesc(ErrorCode::E_LOGIN_FAIL);
+        }
+        echo json_encode($json);
+    }
 }
 ?>
---
+++ src/admin/actions/UserAction.class.php
@@ -0,0 +1,87 @@
+<?php include_once dirname(__FILE__).'/../../common/ErrorCode.class.php';?>
+<?php include_once dirname(__FILE__).'/../../common/entity/UserEntity.class.php';?>
+<?php
+class UserAction extends AjaxAction
+{
+    public function login(){
+        $json['status'] = ErrorCode::E_SUCCESS;
+        
+        $usr = isset($_POST["usrname"]) ? $_POST["usrname"] : null;
+        $pwd = isset($_POST["usrpwd"]) ? $_POST["usrpwd"] : null;
+        //$session = new Session();
+        $json = new stdClass();
+        $json->status = ErrorCode::E_SUCCESS;
+        $entity = new UserEntity();
+        if(empty($usr) || empty($pwd))
+        {
+            $json->status = ErrorCode::E_LOGIN_FAIL;
+            $json->msg = ErrorCode::getErrDesc(ErrorCode::E_LOGIN_FAIL);
+            echo json_encode($json, true);
+            return;
+        }
+        if($entity->auth($usr, $pwd) === true)
+        {
+            $json->status = ErrorCode::E_SUCCESS;
+            $json->msg = ErrorCode::getErrDesc(ErrorCode::E_SUCCESS);
+        }
+        else {
+            $json->status = ErrorCode::E_LOGIN_FAIL;
+            $json->msg = ErrorCode::getErrDesc(ErrorCode::E_LOGIN_FAIL);
+        }
+        echo json_encode($json);
+    }
+    
+    public function registerCheck()
+    {
+        $usr = isset($_POST["usrname"]) ? $_POST["usrname"] : null;
+        $json = new stdClass();
+        $entity = new UserEntity();
+        if(empty($usr)){
+            $json->status = ErrorCode::E_NOT_EXISTS;
+            $json->msg = ErrorCode::getErrDesc(ErrorCode::E_NOT_EXISTS);
+            echo json_encode($json, true);
+            return;
+        }
+        if($entity->checkUserExists($usr) == true)
+        {
+            $json->status = ErrorCode::E_IS_EXISTS;
+            $json->msg = ErrorCode::getErrDesc(ErrorCode::E_IS_EXISTS);
+        }
+        else {
+            $json->status = ErrorCode::E_SUCCESS;
+            $json->msg = ErrorCode::getErrDesc(ErrorCode::E_SUCCESS);
+        }
+        echo json_encode($json);
+    }
+    
+    public function register(){
+        $usr = isset($_POST["usrname"])?$_POST["usrname"]:null;
+        $pwd = isset($_POST["usrpwd"])?$_POST["usrpwd"]:null;
+        $name = isset($_POST["usrrealname"])?$_POST["usrrealname"]:null;
+        $tele = isset($_POST["usrtele"])?$_POST["usrtele"]:null;
+        $zipCode = isset($_POST["usrzipcode"])?$_POST["usrzipcode"]:null;
+        $addr = isset($_POST["usraddr"])?$_POST["usraddr"]:null;
+        $json = new stdClass();
+        
+        if(empty($usr) || empty($pwd) || empty($name) ||empty($tele))
+        {
+            $json->status = ErrorCode::E_INPUT_ERROR;
+            $json->msg = ErrorCode::getErrDesc(ErrorCode::E_INPUT_ERROR);
+            echo json_encode($json);
+            return;
+        }
+
+        $entity = new UserEntity();
+        if($entity->add($usr, $pwd, $name, $tele, $zipCode, $addr) == true)
+        {
+            $json->status = ErrorCode::E_SUCCESS;
+            $json->msg = ErrorCode::getErrDesc(ErrorCode::E_SUCCESS);
+        }
+        else {
+            $json->status = ErrorCode::E_DB_ERR;
+            $json->msg = ErrorCode::getErrDesc(ErrorCode::E_DB_ERR);
+        }
+        echo json_encode($json);     
+    }
+}
+?>
