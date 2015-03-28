c4ef7898412c653c2823a1f05fed7271514ccdb1
hqbobo <29414@qq.com>
2015-03-28 16:24:03 +0800

    [提交订单页面基础版本完成]
    Signed-off-by: hqbobo <29414@qq.com>

--- src/app/js/order.js
+++ src/app/js/order.js
@@ -1,4 +1,25 @@
 (function($) {
+	var fix = true;
+	$(window).scroll(function () {
+
+		if( $(document).height() - $(window).scrollTop() < 900)
+		{
+			  $('.checkout-buttons').removeClass('ui-ceilinglamp-current');
+			  fix= false;
+		}
+		else
+		{
+			if(fix ==false)
+			{
+				$('.checkout-buttons').addClass('ui-ceilinglamp-current');
+				fix = true;
+			}
+		}
+//	       if ($(window).scrollTop() == $(document).height() - $('.footer').height()) {
+//	           alert('bottom!!');
+//
+//	       }
+	 });
 	$('#payment-list li').hover(function(){
 		$(this).find('.payment-item').addClass('payment-item-hover');
 	},
@@ -27,5 +48,27 @@
 	function(){
 		$('.qmarkTip').hide();
 	});
+	$('.mode-tab-nav li').hover(function(){
+
+		$(this).addClass('hover');
+	},
+	function(){
+		$(this).removeClass('hover');
+	});
 	
+	$('.mode-tab-nav li .qmark-tip').hover(function(){
+
+		var X = $(this).offset().top;
+		var Y = $(this).offset().left; 
+		var tip = $('.qmarkTip');
+		var tipInner = tip.find('.ui-tips-main');
+		//alert($(this).height()+"="+ $(this).width());
+		tip.css('top', X + $(this).height());
+		tip.css('left', Y + $(this).width()-29);
+		$().html($(this).attr('data-tips'));
+		tip.show();
+	},
+	function(){
+		$('.qmarkTip').hide();
+	});
 })(jQuery);
