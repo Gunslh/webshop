ea6d61340b2d32bc8e95cd6729b90f4cd87fde06
hqbobo <krhu@5CG445047W.nsn-intra.net>
2015-04-10 15:47:03 +0800

    [customer action auth bug fix add 2 white list]
    Signed-off-by: hqbobo <krhu@5CG445047W.nsn-intra.net>

--- src/customer/filters/LoginFilter.class.php
+++ src/customer/filters/LoginFilter.class.php
@@ -7,10 +7,32 @@ class LoginFilter extends BaseFilter
 {
 	const LOGIN_ACTION_URL = 'customer/UserAction/login';
 	const LOGIN_PAGE_URL = 'login.html';
+	public static $filters = Array(
+			1=>'/customer/ProductSeekAction/selectAllMenu',
+			2=>'/customer/ProductSeekAction/getCateInfo',
+			3=>'/customer/ProductSeekAction/selectAllCategory',
+			4=>'/customer/ProductSeekAction/getSubmenuByCatId',
+			5=>'/customer/ProductSeekAction/getProductCountByCatId',
+			6=>'/customer/ProductSeekAction/getProductByCatId',
+			7=>'/customer/actions/ProductSeekAction/getProductByCatId',
+			8=>'/customer/actions/ProductSeekAction/getProductCountByCatId',
+			9=>'/customer/actions/ProductSeekAction/getProductMedia',
+			10=>'/customer/CartAction/watchCart',
+			11=>'/customer/actions/ProductSeekAction/getProductBySubId',
+			12=>'/customer/actions/ProductSeekAction/getProductCountBySubmenuId',
+			);
 	
 	private function whitelistFilter(BaseRequest $request){
+		
 	    if($request->geturl() === WEB_ROOT.self::LOGIN_ACTION_URL){
 	        return true;
+	    }
+	    foreach (LoginFilter::$filters as  $filter)
+	    {
+	    	if($request->geturl() === $filter)
+	    	{
+	    		return true;
+	    	}
 	    }
 	    return false;
 	}
@@ -22,13 +44,12 @@ class LoginFilter extends BaseFilter
 	        $json->msg = ErrorCode::getErrDesc(ErrorCode::E_PERMISSIONS);
 	        echo json_encode($json);
 	    }else{
-	        $response->redirect(self::LOGIN_PAGE_URL);
+	        //$response->redirect(self::LOGIN_PAGE_URL);
 	    }
 	}
 	
 	public function beforeFilter(BaseRequest $request, BaseResponse $response)
 	{
-		return true;
 	    if($this->whitelistFilter($request)){
 	        return true;
 	    }
